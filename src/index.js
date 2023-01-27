d56174d50f582214d15fa289fdbc33ce08c7205e
LuisV <luisfvilla012@gmail.com>
2023-01-26 16:59:50 -0800

    added a dark mode and light mode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
 import {closeProjectForm, displayProjectForm, submitProjectForm} from "./modules/project";
 import { displayAllTaskList, displayImportantList, displayTodayTasks, displayWeekTasks } from "./modules/homeFunctions";
 
+
+
+
+
+const themeBtn = document.getElementById("themeBtn");
+const root = document.querySelector(":root");
+
+themeBtn.addEventListener( "click", () => {
+    if (themeBtn.innerHTML == "Day") {
+        themeBtn.innerHTML = "Night";
+        root.style.setProperty('--blue', '#FFFFFF');
+
+        root.style.setProperty('--white', '#14213D');
+    
+        root.style.setProperty('--black', '#E5E5E5');
+
+        root.style.setProperty('--platinum', '#FCA311');
+
+        root.style.setProperty('--yellow', '#E5E5E5');
+    } else {
+        themeBtn.innerHTML = "Day";
+        root.style.setProperty('--blue', '#14213D');
+
+        root.style.setProperty('--white', '#FFFFFF');
+    
+        root.style.setProperty('--black', '#000000');
+
+        root.style.setProperty('--platinum', '#E5E5E5');
+
+        root.style.setProperty('--yellow', '#FCA311');
+    }
+
+
+}) ;
+
+
+
+
+
 //functions that run for the "ADD PROJECT BUTTON"
 displayProjectForm();
 submitProjectForm();
@@ -10,4 +49,3 @@ displayAllTaskList();
 displayTodayTasks();
 displayWeekTasks();
 displayImportantList();
-
